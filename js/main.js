b856e696a7e313029ecf8d6bde309e2e35200025
Alexey <koroza.alexey99@gmail.com>
2022-03-25 00:11:06 +0300

    *

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 $(function () {
-    $('.single__item').slick();
-  /* $('.slider').slick({
+  $('.slider').slick({
     dots: true,
-    infinite: true,
-    speed: 300,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="images/prev.svg" alt="prev.svg"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="images/next.svg" alt="next.svg"></button>',
+    infinite: false,
     slidesToShow: 1,
-    adaptiveHeight: true,
-  }); */
+    adaptiveHeight: false,
+  });
 });
