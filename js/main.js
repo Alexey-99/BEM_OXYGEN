c7ab9e501158745cbf813ac018529b103ee1d6b9
Alexey <koroza.alexey99@gmail.com>
2022-03-24 22:17:22 +0300

    *

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 $(function () {
-  $('.slider').slick({
+    $('.single__item').slick();
+  /* $('.slider').slick({
     dots: true,
     infinite: true,
     speed: 300,
     slidesToShow: 1,
     adaptiveHeight: true,
-  });
+  }); */
 });
