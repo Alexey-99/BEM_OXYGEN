6f1eb0820deb7960539f24621cc9e294c8ebf19a
Alexey <koroza.alexey99@gmail.com>
2022-03-25 21:50:36 +0300

    *

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $(function () {
       '<button type="button" class="slick-prev"><img src="images/prev.svg" alt="prev.svg"></button>',
     nextArrow:
       '<button type="button" class="slick-next"><img src="images/next.svg" alt="next.svg"></button>',
-    infinite: false,
+    infinite: true,
     slidesToShow: 1,
     adaptiveHeight: false,
   });
